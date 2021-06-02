effaa7521865270f8061b26de80629a8fc99bc97
Akirakii <akiraki.awa@gmail.com>
2021-06-02 15:35:29 -0500

    some implementations on inventory-service

---
+++ domain/inventory-service/inventory/src/main/java/com/paulsoft/inventory/dtos/ProductCategoryDto/CreateProductCategoryDto.java
@@ -0,0 +1,11 @@
+package com.paulsoft.inventory.dtos.ProductCategoryDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateProductCategoryDto {
+    private String name;
+    private String description;
+}
